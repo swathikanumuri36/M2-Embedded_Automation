5e9880b690176d633c72debf318c0569335ec003
swathikanumuri36 <94305490+swathikanumuri36@users.noreply.github.com>
2021-12-02 00:40:48 +0530

    Update A2_input.c

--- 3_Implementation/src/A2_input.c
+++ 3_Implementation/src/A2_input.c
@@ -1,31 +1,24 @@
-#include <avr/io.h>
-#include <util/delay.h>
-#include "A1_SeatDriverInput.h"
+#include "A2_input.h"
 
-void ports(){
-    DDRB|=(1<<PB0);
-
-    DDRD&=~(1<<PD0); //direction register   //clear bit
-    PORTD|=(1<<PD0); //pin                  //set as HIGH
-
-    DDRD&=~(1<<PD1); //direction register   //clear bit
-    PORTD|=(1<<PD1); //pin                  //set as HIGH
+uint16_t chk_value(void){
+      startADC();
+      uint16_t t;
+      t=readADC(0);
+      return t;
 }
 
-int chk_inputs(){
-    ports();
-
-    if(!(PIND&(1<<SEAT_SWITCH)) && !(PIND&(1<<HEAT_SWITCH))){
-        return 1;
-    }
-    else{
-        return 0;
-    }
+void startADC(){
+    ADMUX=(1<<REFS0);
+    ADCSRA= (1<<ADEN)|(7<<ADPS0);
 }
 
-void sysON(){
-    IN_B|=(1<<LEDi);
-}
-void sysOFF(){
-    IN_B&=~(1<<LEDi);
+uint16_t readADC(uint8_t ch){
+    ADMUX&=0xf8;
+    ch=ch&0b00000111;
+    ADMUX|=ch;
+
+    ADCSRA|=(1<<ADSC);
+    while(!(ADCSRA & (1<<ADIF)));
+    ADCSRA|=(1<<ADIF);
+    return(ADC);
 }
