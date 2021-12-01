a14ba8442d0babf98262b0aa96681ad68d23280a
swathikanumuri36 <94305490+swathikanumuri36@users.noreply.github.com>
2021-12-02 00:36:24 +0530

    Create main.c

---
+++ 3_Implementation/main.c
@@ -0,0 +1,29 @@
+/*
+ */
+#include <avr/io.h>
+#include <util/delay.h>
+#include "A1_SeatDriverInput.h"
+#include "A2_input.h"
+
+int main(void)
+{
+    ports();
+    uint16_t t;
+    int seat_heat_switch=0;
+
+    while(1)
+    {
+        seat_heat_switch=chk_inputs();
+        if(seat_heat_switch==1){
+            sysON();
+            t=chk_value();
+            PWMgen(t);
+        }
+        else{
+            sysOFF();
+        }
+    }
+
+
+    return 0;
+}
