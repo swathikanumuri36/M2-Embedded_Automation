f69f5c314358c7191b5e69eccccf0be65cd5d438
swathikanumuri36 <94305490+swathikanumuri36@users.noreply.github.com>
2021-12-02 00:31:56 +0530

    Update A3_PWMgen.h

--- 3_Implementation/inc/A3_PWMgen.h
+++ 3_Implementation/inc/A3_PWMgen.h
@@ -1,1 +1,27 @@
 
+#ifndef A3_PWMGEN_H_INCLUDED
+#define A3_PWMGEN_H_INCLUDED
+
+//Output PWM for 20% duty cycle
+//Output PWM for 40% duty cycle
+//Output PWM for 70% duty cycle
+//Output PWM for 95% duty cycle
+
+#define PWM20_20C (205)
+#define PWM40_25C (410)
+#define PWM70_29C (717)
+#define PWM95_33C (973)
+
+#include <util/delay.h>
+#include <avr/io.h>
+
+void timerON();
+
+void PWMgen(uint16_t t);
+
+void dispLevel1();
+void dispLevel2();
+void dispLevel3();
+void dispLevel4();
+
+#endif // A3_PWMGEN_H_INCLUDED
