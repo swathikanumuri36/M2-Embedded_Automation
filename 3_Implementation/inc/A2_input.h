e9b5fcf23d0ae392e9322a9eea94502a7f02f76d
swathikanumuri36 <94305490+swathikanumuri36@users.noreply.github.com>
2021-12-02 00:31:08 +0530

    Update A2_input.h

--- 3_Implementation/inc/A2_input.h
+++ 3_Implementation/inc/A2_input.h
@@ -1,1 +1,14 @@
 
+#ifndef A2_INPUT_H_INCLUDED
+#define A2_INPUT_H_INCLUDED
+
+#include <util/delay.h>
+#include <avr/io.h>
+
+void startADC();
+
+uint16_t readADC(uint8_t ch);
+
+uint16_t chk_value(void);
+
+#endif // A2_INPUT_H_INCLUDED
