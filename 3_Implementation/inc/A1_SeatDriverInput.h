475c0deb954b6ba8a6c66959a324c5e8fe94fb21
swathikanumuri36 <94305490+swathikanumuri36@users.noreply.github.com>
2021-12-02 00:30:19 +0530

    Update A1_SeatDriverInput.h

--- 3_Implementation/inc/A1_SeatDriverInput.h
+++ 3_Implementation/inc/A1_SeatDriverInput.h
@@ -1,1 +1,18 @@
 
+#ifndef A1_SEATDRIVERINPUT_H_INCLUDED
+#define A1_SEATDRIVERINPUT_H_INCLUDED
+
+#define IN_B (PORTB)
+#define LEDi (PORTB0)
+#define SEAT_SWITCH (PORTD0)
+#define HEAT_SWITCH (PORTD1)
+
+void ports();
+
+void sysON();
+
+void sysOFF();
+
+int chk_inputs();
+
+#endif // A1_SEATDRIVERINPUT_H_INCLUDED
